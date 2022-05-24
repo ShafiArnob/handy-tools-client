853aad678005d2ce334c03d56d14b20d501ca8a5
ShafiArnob <shafiarnob@gmail.com>
2022-05-25 02:50:52 +0600

    banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,18 @@
+import './Banner.css'
+import bannerImg from '../../../Images/banner1.jpg'
+const Banner = () => {
+  return (
+    <div class="hero min-h-fit" style={{backgroundImage: `url(${bannerImg})`}}>
+      <div class="hero-overlay bg-black opacity-50"></div>
+      <div class="hero-content text-center text-white p-20">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+          <p class="mb-5">Get The best Tools and Hardware in Town</p>
+          <button class="btn btn-primary text-white font-bold">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
