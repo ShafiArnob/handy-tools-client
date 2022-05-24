23ec5032ea513a2c776329d277f2a6b2bdf95c38
ShafiArnob <shafiarnob@gmail.com>
2022-05-25 01:49:55 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Shared/Header';
+import Home from './Pages/Home/Home'
+import Dashboard from './Pages/Dashboard/Dashboard'
+import Blogs from './Pages/Blogs/Blogs'
+import MyPortfolio from './Pages/MyPortfolio/MyPortfolio'
+import Login from './Pages/Login/Login'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Tools</h1>
+    <div>
+      <Header></Header>
+
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/portfolio' element={<MyPortfolio></MyPortfolio>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
-}
+} 
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#06C167",
+          secondary: "#f6d860",
+          accent: "#000000",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      // "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
 }
