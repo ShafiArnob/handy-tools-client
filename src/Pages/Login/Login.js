1c5ae6bc070a3f27f2a75c9e2cf701aa2647113c
ShafiArnob <shafiarnob@gmail.com>
2022-05-25 22:48:00 +0600

    login ui

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content flex-col">
+        <div class="card flex-shrink-0 w-96 shadow-2xl bg-base-100">
+          <h1 className='text-4xl font-bold text-center p-6'>Login</h1>
+          <div class="card-body">
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input type="text" placeholder="email" class="input input-bordered" />
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input type="text" placeholder="password" class="input input-bordered" />
+              <label class="label">
+                <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+              </label>
+            </div>
+            <div class="form-control mt-6">
+              <button class="btn btn-primary text-white font-bold">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
