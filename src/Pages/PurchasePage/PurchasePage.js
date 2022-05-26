d9229c322083c3cf01970118d25b36549b2d4dc2
ShafiArnob <shafiarnob@gmail.com>
2022-05-26 22:25:00 +0600

    quantity error show

--- src/Pages/PurchasePage/PurchasePage.js
+++ src/Pages/PurchasePage/PurchasePage.js
@@ -6,26 +6,43 @@ import auth from '../../firebase.config';
 const PurchasePage = () => {
   const {id} = useParams()
   const [product, setProduct] = useState({})
+  const [inputDisabled, setInputDisabled] = useState(true)
   const [user] = useAuthState(auth);
   useEffect(()=>{
     const url = `http://localhost:5000/products/${id}`
     fetch(url).then(res=>res.json()).then(data=>setProduct(data))
   },[])
 
-  const handlePlaceorder = e =>{
+  const handlePlaceOrder = e =>{
     e.preventDefault()
     const name = e.target.name.value
     const email = e.target.email.value
     const address = e.target.address.value
     const phone = e.target.phone.value
-    const quantity = e.target.quantity.value
-
+    
     const productName = product.name
     const productId = product._id
+    const quantity = e.target.quantity.value
+    const paymentStatus = true
 
-    
+    url = 
   }
   
+  function qty(e){
+    const qty = parseInt(e.target.value)
+
+    if(qty){
+      if(parseInt(product.minQuantity)>qty){
+        setInputDisabled(false)
+      }
+      else{
+        setInputDisabled(true)
+      }
+    }
+    else{
+      setInputDisabled(false)
+    }
+  }
   return (
     <div class="hero min-h-screen bg-base-200 flex flex-row">
       <div class="hero-content flex lg:flex-row p-12 my-6 bg-white rounded-lg m-10">
@@ -39,13 +56,22 @@ const PurchasePage = () => {
         </div>
       </div>
       <div class="hero-content p-12 bg-white rounded-lg w-100 mb-10 m-10">
-        <form className='flex flex-col'>
-          <input type="text" name='name' placeholder="Username" value={user.displayName} class="input input-bordered w-full max-w-xs my-2" disabled/>
-          <input type="text" name='email' placeholder="Email" value={user.email} class="input input-bordered w-full max-w-xs my-2" disabled/>
-          <input type="text" name='address' placeholder="Address" class="input input-bordered w-full max-w-xs my-2" />
+        <form onSubmit={handlePlaceOrder} className='flex flex-col'>
+          <input type="text" name='name' placeholder="Username" value={user?.displayName} class="input input-bordered w-full max-w-xs my-2" disabled/>
+          <input type="text" name='email' placeholder="Email" value={user?.email} class="input input-bordered w-full max-w-xs my-2" disabled/>
+          <textarea type="text" name='address' placeholder="Address" class="input input-bordered w-full max-w-xs my-2" />
           <input type="text" name='phone' placeholder="Phone Number" class="input input-bordered w-full max-w-xs my-2" />
-          <input type="text" name='quantity' placeholder="Quantity" class="input input-bordered w-full max-w-xs my-2" />
-          <button class="btn btn-primary w-80 text-white font-bold mt-6" >Place Order</button>
+          {inputDisabled ? 
+          ''
+          :
+          <p className='p-0 m-0 text-xs text-red-600'>Minimum Quantity {product.minQuantity} and Maximum Quantity {product.quantity}</p>}
+          <input type="number" onChange={qty} name='quantity' placeholder="Quantity" class="input input-bordered w-full max-w-xs my-2" />
+          {
+            inputDisabled? 
+            <button class="btn btn-primary w-80 text-white font-bold mt-6">Place Order</button>
+            :
+            <button class="btn btn-primary w-80 text-white font-bold mt-6" disabled>Place Order</button>
+          }
         </form>
       </div>
     </div>
