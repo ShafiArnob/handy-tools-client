b8a0b13d092858139b9e06a726a190e0831009c6
ShafiArnob <shafiarnob@gmail.com>
2022-05-27 08:34:25 +0600

    my profile ui

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const MyProfile = () => {
+
+  const handleUpdateProfile = () =>{
+
+  }
   return (
-    <div>
-      Myprofile
+    <div class="card w-1/2 flex flex-col items-center bg-base-100  p-4">
+      <form onSubmit={handleUpdateProfile} class="card-body flex flex-col items-center w-full outline outline-1 outline-primary rounded-2xl">
+        <h2 class="text-4xl text-center text-primary p-2">Profile</h2>
+        <input type="text" name='name' value = 'Arob' class="input input-bordered w-full max-w-xs my-2" disabled/>
+        <input type="text" name='email' value= 'a@gmail.com' class="input input-bordered w-full max-w-xs  my-2" disabled/>
+        <textarea type="text" name='address' placeholder="Address" class="input input-bordered w-full max-w-xs my-2" />
+        <input type="text" name='linkedin' placeholder="Linkedin Profile" class="input input-bordered w-full max-w-xs my-2" />
+
+        <button type='submit' class="btn btn-outline btn-primary w-1/3">Update</button>
+      </form>
     </div>
   );
 };
