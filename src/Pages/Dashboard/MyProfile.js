baa4d6fd31d8fa0d2efdd36db5770dd354a3d0d2
ShafiArnob <shafiarnob@gmail.com>
2022-05-28 20:42:08 +0600

    myprofile

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -2,9 +2,16 @@ import React, { useEffect, useState } from 'react';
 import auth from '../../firebase.config';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import Loading from '../../Shared/Loading'
+import { useForm } from "react-hook-form";
+
+
 const MyProfile = () => {
   const [profile,setProfile] = useState({})
   const [user] = useAuthState(auth);
+
+  const { register, handleSubmit, setValue } = useForm();
+  const onSubmit = data => console.log(data)
+
   useEffect(()=>{
     fetch(`http://localhost:5000/profile/${user?.email}`)
     .then(res => res.json())
@@ -43,12 +50,12 @@ const MyProfile = () => {
     <div class="card w-1/2 flex flex-col items-center bg-base-100  p-4">
       <form onSubmit={handleUpdateProfile} class="card-body flex flex-col items-center w-full outline outline-1 outline-primary rounded-2xl">
         <h2 class="text-4xl text-center text-primary p-2">Profile</h2>
-        <input type="text" name='name' value = {user?.displayName} class="input input-bordered w-full max-w-xs my-2" disabled/>
-        <input type="text" name='email' value= {user?.email} class="input input-bordered w-full max-w-xs  my-2" disabled/>
-        <input type="text" name='education' value={profile.education} placeholder="Education" class="input input-bordered w-full max-w-xs my-2" />
-        <input type="text" name='phone' value={profile.phone} placeholder="Phone Number" class="input input-bordered w-full max-w-xs my-2" />
-        <textarea type="text" name='address' value={profile.address} placeholder="Address" class="input input-bordered w-full max-w-xs my-2" />
-        <input type="text" name='linkedin' value={profile.linkedin} placeholder="Linkedin Profile" class="input input-bordered w-full max-w-xs my-2" />
+        <input  type="text" name='name' defaultValue = {user?.displayName} class="input input-bordered w-full max-w-xs my-2" disabled/>
+        <input type="text" name='email' defaultValue = {user?.email} class="input input-bordered w-full max-w-xs  my-2" disabled/>
+        <input defaultValue={profile.education} type="text" name='education' placeholder="Education" class="input input-bordered w-full max-w-xs my-2" />
+        <input type="text" name='phone' defaultValue={profile.phone} placeholder="Phone Number" class="input input-bordered w-full max-w-xs my-2" />
+        <textarea type="text" name='address' defaultValue={profile.address} placeholder="Address" class="input input-bordered w-full max-w-xs my-2" />
+        <input type="text" name='linkedin' defaultValue={profile.linkedin} placeholder="Linkedin Profile" class="input input-bordered w-full max-w-xs my-2" />
 
         <button type='submit' class="btn btn-outline btn-primary w-1/3">Update</button>
       </form>
