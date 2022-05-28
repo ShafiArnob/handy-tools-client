a712152872d0448170ec8ac9a65bd493736c551e
ShafiArnob <shafiarnob@gmail.com>
2022-05-28 21:12:50 +0600

    footer

---
+++ src/Pages/Home/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-900 h-36">
+        <p className="text-center p-10 "><small className='text-white'>© 2020 . All rights reserved by Handy Tools</small></p>
+  </footer>
+  );
+};
+
+export default Footer;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Footer from './Footer';
 import Products from './Products'
 const Home = () => {
   return (
     <div className='bg-gray-100'>
       <Banner></Banner>
       <Products></Products>
+      <Footer></Footer>
     </div>
   );
 };
